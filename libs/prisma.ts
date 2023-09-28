bbd9e827e339a7d91c02374e639ae78069795dc2
luisdoc19 <luisdavidcuello12345@gmail.com>
2023-09-27 23:11:48 -0500

    Errors

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -4,6 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export { prisma };
