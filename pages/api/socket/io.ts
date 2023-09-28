ee341f4822f3ee2f8ae2ac0a921d59f2c4800fc8
luisdoc19 <luisdavidcuello12345@gmail.com>
2023-09-28 00:15:01 -0500

    Errors

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -12,10 +12,8 @@ export const config = {
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
     const path = "/api/socket/io";
-    const httpServer: NetServer = res.socket.server as any;
-    const io = new ServerIO(httpServer, {
+    const io = new ServerIO(res.socket.server, {
       path: path,
-      //@ts-ignore
       addTrailingSlash: false,
     });
     res.socket.server.io = io;
